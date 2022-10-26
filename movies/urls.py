ccafad03c29401794ebcbb5fe4ec083d7660ea06
GG <sudodebug@gmail.com>
2022-10-26 07:48:27 +0500

    Revert "init"
    This reverts commit 80ec0633

--- movies/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.MoviesView.as_view()),
-    path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
-]
