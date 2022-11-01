85fde9d82e6497f4d51cc9657eb53be4ae30013e
GG <sudodebug@gmail.com>
2022-11-01 11:44:55 +0500

    Create admin category

--- movies/admin.py
+++ movies/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Category, Actor, Genre, Movie, MovieShots, RatingStar, Rating, Reviews
 
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "url")
+    list_display_links = ("name",)
+
+
+# admin.site.register(Category, CategoryAdmin)
 admin.site.register(Actor)
 admin.site.register(Genre)
 admin.site.register(Movie)
@@ -10,5 +16,4 @@ admin.site.register(MovieShots)
 admin.site.register(RatingStar)
 admin.site.register(Rating)
 admin.site.register(Reviews)
-
 
