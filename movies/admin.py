d5d6fe703e3dc99cf6f40000094741b65ecefa96
GG <sudodebug@gmail.com>
2022-11-01 11:55:49 +0500

    Create MovieAdmin, ReviewAdmin

--- movies/admin.py
+++ movies/admin.py
@@ -8,12 +8,25 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ("name",)
 
 
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "year")
+    search_fields = ("title", "category__name")
+
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "parent", "movie", "id")
+    readonly_fields = ("name", "email")
+
+
 # admin.site.register(Category, CategoryAdmin)
 admin.site.register(Actor)
 admin.site.register(Genre)
-admin.site.register(Movie)
+# admin.site.register(Movie)
 admin.site.register(MovieShots)
 admin.site.register(RatingStar)
 admin.site.register(Rating)
-admin.site.register(Reviews)
+# admin.site.register(Reviews)
 
