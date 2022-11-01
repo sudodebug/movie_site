4ba692f66798fd0aaf973320072158abb3c5e7ef
GG <sudodebug@gmail.com>
2022-11-01 15:26:23 +0500

    configured MovieAdmin

--- movies/admin.py
+++ movies/admin.py
@@ -8,15 +8,45 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ("name",)
 
 
+class ReviewInline(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ("name", "email")
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "url", "draft")
     list_filter = ("category", "year")
     search_fields = ("title", "category__name")
+    inlines = [ReviewInline]
+    save_on_top = True
+    save_as = True
+    list_editable = ("draft",)
+    # fields = (("actors", "directors", "genres"),)
+    fieldsets = (
+        (None, {
+            "fields": (("title", "tagline"),)
+        }),
+        (None, {
+            "fields": ("description", "poster")
+        }),
+        (None, {
+            "fields": ("year", "world_premiere", "country")
+        }),
+        (None, {
+            "fields": (("actors", "directors", "genres", "category"),)
+        }),
+        (None, {
+            "fields": (("budget", "fees_in_usa", "fees_in_world"),)
+        }),
+        (None, {
+            "fields": (("url", "draft"),)
+        }),
+    )
 
 
 @admin.register(Reviews)
-class ReviewsAdmin(admin.ModelAdmin):
+class ReviewAdmin(admin.ModelAdmin):
     list_display = ("name", "email", "parent", "movie", "id")
     readonly_fields = ("name", "email")
 
