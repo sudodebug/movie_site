208a6225996ce6c1310f09cacd94016f984e7376
GG <sudodebug@gmail.com>
2022-11-02 09:10:31 +0500

    Congfigurate poster at admin

--- movies/admin.py
+++ movies/admin.py
@@ -16,21 +16,33 @@ class ReviewInline(admin.TabularInline):
     readonly_fields = ("name", "email")
 
 
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="80" height="90"')
+
+    get_image.short_description = "img"
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ("title", "category", "url", "draft")
     list_filter = ("category", "year")
     search_fields = ("title", "category__name")
-    inlines = [ReviewInline]
+    inlines = [MovieShotsInline, ReviewInline]
     save_on_top = True
     save_as = True
     list_editable = ("draft",)
+    readonly_fields = ("get_image",)
     fieldsets = (
         (None, {
             "fields": (("title", "tagline"),)
         }),
         (None, {
-            "fields": ("description", "poster")
+            "fields": ("description", ("poster", "get_image"))
         }),
         (None, {
             "fields": (("year", "world_premiere", "country"),)
@@ -47,6 +59,10 @@ class MovieAdmin(admin.ModelAdmin):
         }),
     )
 
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="80" height="90"')
+
+    get_image.short_description = "Movie poster"
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
