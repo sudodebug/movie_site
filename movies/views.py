db8152a695cad3e92985badcde7a75879169c927
GG <sudodebug@gmail.com>
2022-11-03 10:38:27 +0500

    Create genre sidebar

--- movies/views.py
+++ movies/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
-from .models import Movie, Category, Actor
+from .models import Movie, Category, Actor, Genre
 from .forms import ReviewForm
 
 
-class MoviesView(ListView):
+class GenreYear:
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=True)
+
+
+class MoviesView(GenreYear, ListView):
     """Movie list"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     """Movie discription"""
     model = Movie
     slug_field = 'url'
@@ -31,7 +39,7 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     """Actor information output"""
     model = Actor
     template_name = "movies/actors.html"
